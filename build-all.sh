060be564722e59ba2951bf2a820655d28a270dc1
rnulens <raf.nulens@gmail.com>
2017-12-25 14:20:00 +0100

    initial setup - core components

--- build-all.sh
+++ build-all.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-function note() {
-    local GREEN NC
-    GREEN='\033[0;32m'
-    NC='\033[0m' # No Color
-    printf "\n${GREEN}$@  ${NC}\n" >&2
-}
+#function note() {
+#    local GREEN NC
+#    GREEN='\033[0;32m'
+#    NC='\033[0m' # No Color
+#    printf "\n${GREEN}$@  ${NC}\n" >&2
+#}
 
 set -e
 
@@ -18,14 +18,14 @@ cd microservices/core/recommendation-service;         note "Building recommendat
 cd microservices/core/review-service;                 note "Building review...";          ./gradlew clean build; cd -
 cd microservices/composite/product-composite-service; note "Building composite...";       ./gradlew clean build; cd -
 
-cd microservices/support/auth-server;                 note "Building auth...";            ./gradlew clean build; cd -
-cd microservices/support/config-server;               note "Building config...";          ./gradlew clean build; cd -
-cd microservices/support/discovery-server;            note "Building discovery...";       ./gradlew clean build; cd -
-cd microservices/support/edge-server;                 note "Building edge...";            ./gradlew clean build; cd -
-cd microservices/support/monitor-dashboard;           note "Building monitor...";         ./gradlew clean build; cd -
-cd microservices/support/turbine;                     note "Building turbine...";         ./gradlew clean build; cd -
-cd microservices/support/zipkin-server;               note "Building zipkin...";          ./gradlew clean build; cd -
+#cd microservices/support/auth-server;                 note "Building auth...";            ./gradlew clean build; cd -
+#cd microservices/support/config-server;               note "Building config...";          ./gradlew clean build; cd -
+#cd microservices/support/discovery-server;            note "Building discovery...";       ./gradlew clean build; cd -
+#cd microservices/support/edge-server;                 note "Building edge...";            ./gradlew clean build; cd -
+#cd microservices/support/monitor-dashboard;           note "Building monitor...";         ./gradlew clean build; cd -
+#cd microservices/support/turbine;                     note "Building turbine...";         ./gradlew clean build; cd -
+#cd microservices/support/zipkin-server;               note "Building zipkin...";          ./gradlew clean build; cd -
 
-find . -name *SNAPSHOT.jar -exec du -h {} \;
+#find . -name *SNAPSHOT.jar -exec du -h {} \;
 
-docker-compose build
+#docker-compose build
