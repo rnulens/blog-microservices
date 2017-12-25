45031ba9d5d4d9fc976a2802c81a668f37a1ae89
rnulens <raf.nulens@gmail.com>
2017-12-25 15:31:10 +0100

    re-enable note function

--- build-all.sh
+++ build-all.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-#function note() {
-#    local GREEN NC
-#    GREEN='\033[0;32m'
-#    NC='\033[0m' # No Color
-#    printf "\n${GREEN}$@  ${NC}\n" >&2
-#}
+function note() {
+    local GREEN NC
+    GREEN='\033[0;32m'
+    NC='\033[0m' # No Color
+    printf "\n${GREEN}$@  ${NC}\n" >&2
+}
 
 set -e
 
