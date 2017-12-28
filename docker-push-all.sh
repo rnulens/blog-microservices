0f2c12612a9bfd5870631726e90b045f76053b5d
rnulens <raf.nulens@gmail.com>
2017-12-28 21:14:52 +0100

    included docker build + push

---
+++ docker-push-all.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+
+cd microservices/core/product-service;                note "Packaging product...";         docker build -t product-service .; 
+docker push rnulens/product-service:latest; cd -
+cd microservices/core/recommendation-service;         note "Packaging recommendation...";  docker build -t recommendation-service .;
+docker push rnulens/recommendation-service:latest; cd -
+cd microservices/core/review-service;                 note "Packaging review...";          docker build -t review-service .;
+docker push rnulens/review-service:latest; cd -
+cd microservices/composite/product-composite-service; note "Packaging composite...";       docker build -t product-composite-service .;
+docker push rnulens/product-composite-service:latest; cd -
+
+#cd microservices/support/auth-server;                 note "Building auth...";            ./gradlew clean build; cd -
+#cd microservices/support/config-server;               note "Building config...";          ./gradlew clean build; cd -
+#cd microservices/support/discovery-server;            note "Building discovery...";       ./gradlew clean build; cd -
+#cd microservices/support/edge-server;                 note "Building edge...";            ./gradlew clean build; cd -
+#cd microservices/support/monitor-dashboard;           note "Building monitor...";         ./gradlew clean build; cd -
+#cd microservices/support/turbine;                     note "Building turbine...";         ./gradlew clean build; cd -
+#cd microservices/support/zipkin-server;               note "Building zipkin...";          ./gradlew clean build; cd -
+
+#find . -name *SNAPSHOT.jar -exec du -h {} \;
+
+#docker-compose build
